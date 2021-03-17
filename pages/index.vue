89e383948a8bc2eda2fe8650a6bb838da8c2c032
Maulana Yuseph <maulanayuseph@gmail.com>
2021-03-17 18:11:41 +0700

    indentasi spasi

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,70 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-        <h1 class="h-welcome">Halo Selamat Datang,</h1>
-        <p></p>
-        <h4 class="sb-welcome">Anda berada di halaman Reservasi Kunjungan <b>Jabar Command Center</b>.
+      <h1 class="h-welcome">Halo Selamat Datang,</h1>
+      <p></p>
+      <h4 class="sb-welcome">
+        Anda berada di halaman Reservasi Kunjungan <b>Jabar Command Center</b>.
         Anda dapat mengajukan permohonan kunjungan dengan mengisi data diri 
-        pada <nuxt-link class="link-reservasi" to="reservasi">Form Permohonan Kunjungan</nuxt-link>, atau mengecek status permohonan anda 
-        pada halaman <nuxt-link class="link-reservasi" to="cek-reservasi">Cek Status Permohonan.</nuxt-link></h4>
-       <p></p>
-       <h4 class="qs-welcome">Apa yang ingin anda lakukan ?</h4>
+        pada 
+        <nuxt-link class="link-reservasi" to="reservasi">Form Permohonan Kunjungan</nuxt-link>
+        , atau mengecek status permohonan anda 
+        pada halaman 
+        <nuxt-link class="link-reservasi" to="cek-reservasi">Cek Status Permohonan.</nuxt-link>
+      </h4>
+      <p></p>
+      <h4 class="qs-welcome">Apa yang ingin anda lakukan ?</h4>
     </v-col>
     <v-col cols="12" sm="12" md="12">
     </v-col>
     <v-col cols="12" sm="8" md="6" class="ajukan-permohonan">
-         <v-card
-          class="mx-auto card-ajukan"
-          color="#E6E6E6"
-          max-width="344"
-          height="150"
-          outlined
+      <v-card
+        class="mx-auto card-ajukan"
+        color="#E6E6E6"
+        max-width="344"
+        height="150"
+        outlined
         >
-          <v-list-item three-line>
-            <v-img src="/imghome1.svg" max-height="90" max-width="90"/>
-            <v-list-item-content class="avatar-content">
-              <v-list-item-subtitle class="ajukan-title">
-                <nuxt-link class="link-reservasi" to="reservasi">Permohonan Reservasi Jabar Command Center</nuxt-link>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card>
+        <v-list-item three-line>
+          <v-img src="/imghome1.svg" max-height="90" max-width="90"/>
+          <v-list-item-content class="avatar-content">
+            <v-list-item-subtitle class="ajukan-title">
+              <nuxt-link class="link-reservasi" to="reservasi">Permohonan Reservasi Jabar Command Center</nuxt-link>
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
     </v-col>
-
-   <v-col cols="12" sm="8" md="6" class="cek-permohonan">
-         <v-card
-          class="mx-auto card-permohonan"
-          color="#E6E6E6"
-          max-width="344"
-          height="150"
-          outlined
+    <v-col cols="12" sm="8" md="6" class="cek-permohonan">
+      <v-card
+        class="mx-auto card-permohonan"
+        color="#E6E6E6"
+        max-width="344"
+        height="150"
+        outlined
         >
-          <v-list-item three-line>
-            <v-img src="/imghome2.svg" max-height="90" max-width="90"/>
-            <v-list-item-content class="avatar-content">
-              <v-list-item-subtitle class="ajukan-title">
-                <nuxt-link class="link-reservasi" to="cek-reservasi">Cek Status Permohonan</nuxt-link>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card>
+        <v-list-item three-line>
+          <v-img src="/imghome2.svg" max-height="90" max-width="90"/>
+          <v-list-item-content class="avatar-content">
+            <v-list-item-subtitle class="ajukan-title">
+              <nuxt-link class="link-reservasi" to="cek-reservasi">Cek Status Permohonan</nuxt-link>
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
     </v-col>
     <v-col cols="12" sm="12" md="12"></v-col>
     <v-col cols="12" sm="8" md="6">
-      <h4 class="font-weight-regular">Apabila anda mengalami kesulitan, silahkan menghubungi <nuxt-link class="link-reservasi" to="#">Pusat Bantuan Jabar Command Center</nuxt-link> </h4>
+      <h4 class="font-weight-regular">
+        Apabila anda mengalami kesulitan, silahkan menghubungi 
+        <nuxt-link class="link-reservasi" to="#">Pusat Bantuan Jabar Command Center</nuxt-link>
+      </h4>
     </v-col>
     <v-col cols="12" sm="12" md="12"></v-col>
-    
   </v-row>
 </template>
-
 <script>
-
-export default {
-
-}
+  export default {
+  
+  }
 </script>
