107db90853a0d9522907a797fc814cb2f9e5ec9e
Ida Bagus Wedagama <wardana.wedagama@gmail.com>
2021-04-03 10:33:04 +0700

    add visual improvements to landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="8" offset-md="1">
-      <h1 class="h-welcome pb-5">Halo Selamat Datang,</h1>
-      <h4 class="sb-welcome">
+  <v-row>
+    <v-col cols="12" class="mt-16">
+      <h1 class="text-h5 text-md-h4 font-weight-medium mb-4 primary--text">Halo Selamat Datang,</h1>
+      <h2 class="text-subtitle-1 darkgray--text mb-8">
         Anda berada di halaman Reservasi Kunjungan <b>Jabar Command Center</b>.
         Anda dapat mengajukan permohonan kunjungan dengan mengisi data diri pada
         <nuxt-link class="link-reservasi" to="reservasi"
@@ -12,13 +12,13 @@
         <nuxt-link class="link-reservasi" to="cek-reservasi"
           >Cek Status Permohonan.</nuxt-link
         >
-      </h4>
-      <h4 class="qs-welcome">Apa yang ingin anda lakukan ?</h4>
+      </h2>
+      <h2 class="text-subtitle-1 darkgray--text font-weight-bold mb-4">Apa yang ingin anda lakukan?</h2>
     </v-col>
-    <v-col cols="12" sm="8" md="4" align="center">
+    <v-col cols="12" sm="12" md="6" class="mb-2" sm-justify="center">
       <nuxt-link class="link-reservasi" to="reservasi">
         <v-card
-          class="mx-auto card-ajukan"
+          class="card-ajukan drop-shadow"
           color="#E6E6E6"
           max-width="344"
           height="150"
@@ -28,19 +28,17 @@
             <v-img src="/imghome1.svg" max-height="90" max-width="90" />
             <v-list-item-content class="avatar-content">
               <v-list-item-subtitle class="ajukan-title">
-                <strong class="link-reservasi"
-                  >Permohonan Reservasi</strong
-                >
+                <strong class="link-reservasi primary--text">Permohonan Reservasi</strong>
               </v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
         </v-card>
       </nuxt-link>
     </v-col>
-    <v-col cols="12" sm="8" md="4" align="center">
+    <v-col cols="12" sm="12" md="6" class="mb-2" sm-justify="center">
       <nuxt-link class="link-reservasi" to="cek-reservasi">
         <v-card
-          class="mx-auto card-permohonan"
+          class="card-permohonan drop-shadow"
           color="#E6E6E6"
           max-width="344"
           height="150"
@@ -50,17 +48,16 @@
             <v-img src="/imghome2.svg" max-height="90" max-width="90" />
             <v-list-item-content class="avatar-content">
               <v-list-item-subtitle class="ajukan-title">
-                <strong class="link-reservasi">Cek Status Permohonan</strong>
+                <strong class="link-reservasi primary--text">Cek Status Permohonan</strong>
               </v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
         </v-card>
       </nuxt-link>
     </v-col>
-    <v-col cols="12" align="center" sm="8" md="8">
-      <HelpLink/>
+    <v-col cols="12" class="mt-8">
+      <HelpLink />
     </v-col>
-    <v-col cols="12" sm="12" md="12"></v-col>
   </v-row>
 </template>
 <script>
