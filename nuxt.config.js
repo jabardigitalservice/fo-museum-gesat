ceb8c07ab6b011df94fc6f45579f41c73846660d
Bangun Bagustapa <bangunbagustapa@gmail.com>
2021-04-06 16:26:34 +0700

    Remove title template

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,6 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - reservasi-commandcenter-microsite',
     title: 'Reservasi Jabar Command Center',
     htmlAttrs: {
       lang: 'en'
